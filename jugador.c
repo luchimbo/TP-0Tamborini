c278b2501fd2165c7a36c16433ae7d06b0f3caa9
Lucio Tamborini <59577462+luchimbo@users.noreply.github.com>
2020-06-04 09:58:16 -0300

    Add files via upload

---
+++ jugador.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "demogorgon.h"
+
+const int DEF_ANIMO_JUGADOR = 0;
+const char DEF_NIVEL_CONFIANZA_JUGADOR = 'w';
+const float DEF_FUERZA_PSIQUICA = -1.0F;
+
+
+int main(){
+
+	comenzar_enfrentamiento();
+
+	int animoRecibido
+	printf(MSJ_ANIMO);
+	scanf("%i", &animoRecibido);
+
+	recibir_animo();
+
+return 0;
+}
